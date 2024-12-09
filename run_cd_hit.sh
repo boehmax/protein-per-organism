33f8549ed982e81ca65dba62e2dd4df5cd286007
boehmax <maximilian.bohm@kemi.uu.se>
2024-12-09 16:14:54 +0100

    added git ignore to clean up stuff, and added changes to run_cd_hit, not wokring well so far however

--- run_cd_hit.sh
+++ run_cd_hit.sh
@@ -4,10 +4,53 @@
 cdhit_path="cd-hit"
 
 # Change to the directory provided as an argument
-cd "output/2024-03-06/fasta_sorted_by_species"
+cd "output/2024-09-10/fasta_sorted_by_species"
 
 # Loop over all .fasta files
 for k in *.fasta
 do
   "${cdhit_path}" -i "$k" -o ./fasta_species_clustered/"$k" -c 0.90 -n 5
+done
+
+set -e
+
+# Directory containing .clstr files
+dir="./fasta_species_clustered"
+
+# Output file
+output="cluster_representative.txt"
+
+# Temporary file
+temp=$(mktemp)
+
+# Ensure temporary file is removed on exit
+trap 'rm -f $temp' EXIT
+
+# Remove the output file if it exists
+rm -f $output
+
+# Process each .clstr file in the directory
+for file in $dir/*.clstr
+do
+    # Process each cluster in the file
+    awk '
+    BEGIN { cluster_id = 0 }
+    /^>Cluster/ {
+        cluster_id++
+        main_entry = ""
+        next
+    }
+    {
+        entry = gensub(/^.*>([^,]*),.*$/, "\\1", "g", $0)
+        if ($0 ~ /\*/) {
+            main_entry = entry
+            percentage = "100.00"
+        } else {
+            percentage = gensub(/^.*at ([0-9.]+)%.*$/, "\\1", "g", $0)
+            if (main_entry) {
+                printf "%s\t%s\t%s\n", main_entry, entry, percentage >> "'"$output"'"
+            }
+        }
+    }
+    ' $file
 done
